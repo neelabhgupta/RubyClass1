f3c892d689dc2cd9682a2761762bb889082580ba
neelabh gupta <neelabh.gupta@urbanladder.com>
2015-06-02 17:23:01 +0530

    #comment adding a not equal cas

--- spec/rectangle_spec.rb
+++ spec/rectangle_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
 describe Rectangle do
-	it "takes length and breadth of a Rectangle and returns the perimeter" do
+	it "takes length and breadth of a Rectangle and checks if perimeter is correct" do
 		length = 1 
 		breadth = 1
 		rectangle = Rectangle.new(length, breadth)
 		expect(rectangle.perimeter).to eq(4)
 	end
-end
+
+  it "takes length,breadth of a Rectangle as 4,1 and checks perimeter not equals to 4" do
+    length = 4 
+    breadth = 1
+    rectangle = Rectangle.new(length, breadth)
+    expect(rectangle.perimeter).not_to eq(4)
+  end
+
+end 
