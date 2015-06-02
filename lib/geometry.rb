59c46a78c6ef4c28bee7d02ed1f020d665181755
neelabh gupta <neelabh.gupta@urbanladder.com>
2015-06-02 12:24:31 +0530

    #comment first rspec

--- lib/geometry.rb
+++ lib/geometry.rb
@@ -5,9 +5,11 @@ class Geometry
 		@y1=y1
 		@y2=y2
 	end
+
 	def lenth_of_line
 		length=Math.hypot(@x2-@x1,@y2-@y1)
 	end
+
 	def self.length_with_args(x1,x2,y1,y2)
 		Math.hypot(x2-x1,y2-y1)
 	end
--- spec/geometry_spec.rb
+++ spec/geometry_spec.rb
@@ -1,13 +1,16 @@
 require 'spec_helper'
 
 describe Geometry do
+
 	before :each do
-    	@geom = Geometry.new 1,4,1,5
+    @geom = Geometry.new 1,4,1,5
 	end
-	it "length of line" do
+
+	it "check for the length of a line" do
 		@geom.lenth_of_line.should eql 5.0
 	end
-	it "check for x1" do
+
+	it "check for a length of line with arguments" do
 		length = Geometry.length_with_args(1,4,1,5)
 		expect(length).to eq(5.0)
 	end
