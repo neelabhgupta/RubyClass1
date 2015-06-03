6ea4d2e199be9a31db3643a106754854ba6275ed
neelabh gupta <neelabh.gupta@urbanladder.com>
2015-06-03 11:15:59 +0530

    #comment test case with float

--- spec/square_spec.rb
+++ spec/square_spec.rb
@@ -1,18 +1,18 @@
 require 'spec_helper'
 
 describe Square do
-	it "takes top left co-ordinate opp and side length and checks if perimeter is correct" do
+	it "takes top left co-ordinate and side length of square and calculates perimeter" do
 		point1 = Point.new(0,0) 
 		length = 4
 		square = Square.new(point1, length)
 		expect(square.perimeter).to eq(16)
 	end
 
-  it "takes top left co-ordinate opp and side length and checks if area is correct" do
-    point1 = Point.new(0,0) 
-    length = 4
+  it "takes top left co-ordinate and side length of square and calculates area" do
+    point1 = Point.new(3.0,0) 
+    length = 4.1
     square = Square.new(point1, length)
-    expect(square.area).to eq(16)
+    expect(square.area).to eq(4.1 ** 2)
   end
 
 end 
